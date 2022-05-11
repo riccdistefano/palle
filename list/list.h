8b15d628af8c254e15e1ed898037d55b6cf0dca5
capodellatrap <105050541+riccdistefano@users.noreply.github.com>
2022-05-11 10:11:14 +0200

    Header fix

--- list/list.h
+++ list/list.h
@@ -1,5 +1,3 @@
-
-
 typedef struct
 {
     float *elements;
@@ -7,15 +5,14 @@ typedef struct
     int capacity; // grandezza array
 } dyn_arr;
 
-dyn_arr create(); // works
-void add(dyn_arr *arr, float element); // works
-void remove2(dyn_arr *arr, float element); // works
-void remove_at(dyn_arr *arr, int pos); // works
-void insert(dyn_arr *arr, float element, int pos); // works
-void clear(dyn_arr *arr); // works
-void trim(dyn_arr *arr);  // works
-void set_capacity(dyn_arr *arr, int capacity); // works
-int length(dyn_arr *arr); // works
-void set(dyn_arr *arr, float element, int pos); // works
-float get(dyn_arr *arr, int pos); // works
-int find(dyn_arr *arr, float element); // works
+dyn_arr create();
+void add(dyn_arr *arr, float element);
+void remove2(dyn_arr *arr, float element);
+void remove_at(dyn_arr *arr, int pos);
+void insert(dyn_arr *arr, float element, int pos);
+void clear(dyn_arr *arr);
+void trim(dyn_arr *arr);
+void set_capacity(dyn_arr *arr, int capacity);
+void set(dyn_arr *arr, float element, int pos);
+float get(dyn_arr *arr, int pos);
+int find(dyn_arr *arr, float element);
